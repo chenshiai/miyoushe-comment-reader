316cd67c16d3028470bb7b27b5317b428969521d
chenshiai <1183942503@qq.com>
2026-08-05 19:48:27 +0800

    fix(api): update api base url port to 8088
    修正了API请求的基础URL端口号，将原本无端口的地址改为8088端口

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'https://8.155.52.59',
+  baseURL: 'https://8.155.52.59:8088',
   timeout: 60000,
   headers: {
     'Content-Type': 'application/json'
